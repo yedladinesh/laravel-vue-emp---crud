0589656087ee906f753e4e4b3e7094993caeb6c4
Dinesh <yadladinesh1@gmail.com>
2022-06-23 16:49:46 +0530

    created employee modal and migrate it

---
+++ database/migrations/2022_06_23_110725_create_employees_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmployeesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('empId', 10);
+            $table->string("name",100);
+            $table->string("address", 200);
+            $table->string("email", 200)->unique();
+            $table->number("phone_number");
+            $table->string("dob");
+            $table->string("image");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+}
